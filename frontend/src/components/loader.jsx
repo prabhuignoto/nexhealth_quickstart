c2f66603835b44e5877f8f06343f132853f8a017
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-05-18 21:36:37 +0530

    adding loader (#5)

---
+++ frontend/src/components/loader.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./loader.module.css";
+
+const Loader = ({}) => {
+  return (
+    <>
+      <div className={styles.spinner}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export { Loader };
